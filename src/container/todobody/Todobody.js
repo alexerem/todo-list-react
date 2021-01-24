3fb78f7293d41ac676077f3aec22975a87c4f082
Alexander <band.kist@gmail.com>
2021-01-24 18:06:05 +0300

    Start project, add structure

---
+++ src/container/todobody/Todobody.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Classes from './Todobody.module.css';
+
+export default class Todobody extends Component {
+
+	render() {
+		return (
+			<div className={Classes.Todobody}>
+
+			</div>
+		)
+	}
+}
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Todobody from "../../container/todobody/Todobody";
+
+export default class Layout extends Component {
+
+	render() {
+		return (
+			<Todobody />
+		)
+	}
+}
