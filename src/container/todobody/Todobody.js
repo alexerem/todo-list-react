83939d25c8886ad736f7e072a8ba56d8872160fe
Alexander <band.kist@gmail.com>
2021-01-25 00:38:45 +0300

    Add inputHandler logic, style

--- src/container/todobody/Todobody.js
+++ src/container/todobody/Todobody.js
@@ -1,11 +1,40 @@
 import React, {Component} from 'react';
-import Classes from './Todobody.module.css';
+import classes from './Todobody.module.css';
+import {PlusCircleOutlined} from '@ant-design/icons';
+
 
 export default class Todobody extends Component {
 
+	state = {
+		taskList: [],
+		input: ''
+	}
+
+	changeInput(event) {
+		this.setState({
+			input: event.target.value
+		})
+		console.log(this.state.input)
+	}
+
+	buttonPlusHandler() {
+
+	}
+
 	render() {
 		return (
-			<div className={Classes.Todobody}>
+			<div className={classes.Todobody}>
+				<div className={classes.taskInput}>
+					<label htmlFor="taskInput">Enter you task</label>
+					<input type="text" id="taskInput" onChange={this.changeInput.bind(this)} />
+					<div
+						className={classes.buttonPlus}
+						onClick={this.buttonPlusHandler()}
+					>
+						<PlusCircleOutlined />
+					</div>
+				</div>
+
 
 			</div>
 		)
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import Todobody from "../../container/todobody/Todobody";
+import classes from './Layout.module.css';
+
 
 export default class Layout extends Component {
 
 	render() {
 		return (
-			<Todobody />
+			<div className={classes.Layout}>
+				<Todobody />
+			</div>
 		)
 	}
 }
